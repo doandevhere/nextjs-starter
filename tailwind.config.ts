2c33ad489dc4a3423cbd0fa6c2cc8db02b538c46
doandevhere <tranduongdoan03122001@gmail.com>
2024-08-12 09:45:26 +0700

    refactor: update `tailwind.config.ts`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,12 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
     extend: {},
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [],
 };
 export default config;
